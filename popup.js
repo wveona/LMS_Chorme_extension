fcf5b86c5452c7f2695b0ed4a20186fe4deb7e8e
pakuland3 <pakuland_1@naver.com>
2024-07-03 14:08:06 +0900

    onandoff edit

--- popup.js
+++ popup.js
@@ -1,4 +1,21 @@
-function onAndoff(status){
-    if(status==1) alert('LMS 확장 켜짐');
-    else alert('LMS 확장 꺼짐');
+let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+ // Execute script in the current tab
+function onAndoff(st){
+    if(st==0){
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: () => {
+                alert('LMS 확장 꺼짐')
+            }
+        });
+    }
+    else{
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: () => {
+                alert('LMS 확장 꺼짐')
+            }
+        });
+    }
 }
